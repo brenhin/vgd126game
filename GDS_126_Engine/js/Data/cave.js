2e75cf69324f274ce86a1665e90cef656bd2d6ad
brenhin <146984863+brenhin@users.noreply.github.com>
2024-03-06 14:42:39 -0500

    Add files via upload

---
+++ GDS_126_Engine/js/Data/cave.js
@@ -0,0 +1,303 @@
+var x=false;
+var caveData ={
+	info:{
+		layout:[
+			[x,x,18,19,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,21,22,x,x],
+			[18,19,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,21,22],
+			[11,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,14],
+			[17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
+			[x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
+			[x,x,x,x,x,x,x,24,25,26,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
+			[x,x,x,x,x,x,x,27,x,29,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
+			[39,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,41]
+			
+			
+		],
+		src:`images/tileset.png`,
+	},
+	states:
+	[		
+			{
+				fps:5,
+				cycle:false,
+				frames:[
+					{width:64, height:64, startX:0*64, startY:0}
+				]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:1*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:2*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:3*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:4*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:5*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:6*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:7*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:8*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:9*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:10*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:11*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:12*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:13*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:14*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:15*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:16*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:17*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:18*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:19*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:20*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:21*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:22*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:23*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:24*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:25*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:26*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:27*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:28*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:29*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:30*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:31*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:32*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:33*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:34*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:35*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:36*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:37*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:38*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:39*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:40*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:41*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:42*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:43*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:44*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:45*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:46*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:47*64, startY:0}]
+			},
+			{
+				fps:1,
+				cycle:false,
+				frames:[{width:64, height:64, startX:48*64, startY:0}]
+			}
+		]
+	}
+	var caveBackData ={
+		info:{
+			layout:[
+			[x,x,x,x,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,x,x,x,x],
+			[x,x,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,x,x],
+			[16,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,16],
+			[16,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,16],
+			[16,15,15,23,23,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,16],
+			[16,15,15,23,23,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,16],
+			[16,15,15,23,23,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,16],
+			],
+			src:`images/tileset.png`,
+		},
+		states:caveData.states
+		}
+
+		var caveHitData={
+			info:{
+				layout:[
+					[0,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,2],
+					[2,8,1,8,1,1,8,1,1,1,1,1,1,8,8,1,8,8,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+					[2,1,1,8,1,1,8,1,1,1,1,1,1,8,8,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+					[6,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
+					[x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
+					[x,x,x,x,x,x,x,1,1,1,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
+					[x,x,x,x,x,x,x,1,1,1,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
+					[x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2]
+					
+					
+				],
+				src:`images/bg2.png`,
+			},
+			states:caveData.states
+			
+			}
---
+++ GDS_126_Engine/js/Data/player.js
@@ -0,0 +1,95 @@
+/*----------------------------------------------
+This file contains the data used to render the player's sprites
+Properties:
+	Object: info | information about the sprite file
+		String: info.src | The location of the spritesheet
+	Object: states | contains the data for each animation state
+		Object: [`state name`] | The data used to render the idle state
+			Number: fps | The frame rate in which to render the animation
+			Boolean: cycle | Whether or not the animation will loop
+			Array: frames| Contains objects with geometric data for each frame of animtati.
+					Must contain at least one frame object.
+					The animation will run for however many frame objects are added to the array
+				Object: [index number] | A frame of animation
+					Number: width | the actual 1/1 horizontal size of the portion of image file to be rendered
+					Number: height | the actual 1/1 vertical size of the portion of image file to be rendered
+					Number: startX | the horizontal starting point of the portion of image file to be rendered
+					Nunber: startY | the vertical starting point of the portion of image file to be rendered
+/*----------------------------------------------*/
+
+var playerData ={
+	info:{
+		src:`images/duende.png`
+	},
+	states:{
+		//The idle animation 
+    	idle:
+		{
+			fps:15,
+			cycle:true,
+			frames:
+			[
+				{width:128, height:128, startX:0*128, startY:0},
+				{width:128, height:128, startX:1*128, startY:0},
+				{width:128, height:128, startX:2*128, startY:0},
+				{width:128, height:128, startX:3*128, startY:0},
+				{width:128, height:128, startX:4*128, startY:0},
+				{width:128, height:128, startX:5*128, startY:0},
+				{width:128, height:128, startX:6*128, startY:0},
+				{width:128, height:128, startX:7*128, startY:0}
+				
+			]
+		},
+		//The walwidth:128, height:128,
+		walk:
+		{
+			fps:5,
+			cycle:true,
+			frames:
+			[
+				{width:128, height:128, startX:0*128, startY:128},
+				{width:128, height:128, startX:1*128, startY:128},
+				{width:128, height:128, startX:2*128, startY:128},
+				{width:128, height:128, startX:3*128, startY:128}
+			]
+		},
+		//The jump animation 
+		jump:
+		{
+			fps:15,
+			cycle:false,
+			frames:
+			[
+				{width:128, height:128, startX:0*128, startY:3*128}
+			]
+		},
+		//The crouch animation 
+		crouch:
+		{
+			fps:15,
+			cycle:true,
+			frames:
+			[
+				{width:128, height:128, startX:1*128, startY:3*128}
+			]
+		},
+		//The attack animation 
+		attack:
+		{
+			fps:3,
+			cycle:true,
+			//width:300,
+			frames:
+			[
+				{width:128, height:128, startX:0*128, startY:2*128},
+				{width:128, height:128, startX:1*128, startY:2*128},
+				{width:128, height:128, startX:2*128, startY:2*128},
+				{width:128, height:128, startX:3*128, startY:2*128},
+				{width:128, height:128, startX:4*128, startY:2*128},
+				{width:128, height:128, startX:5*128, startY:2*128}
+				
+			]
+		}
+	}
+		
+}
